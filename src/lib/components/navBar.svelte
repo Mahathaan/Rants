cf0ab8ea798598c64f76649ec442d9f939bc117e
HARI PRASANTH S <hariprasanth581@gmail.com>
2024-10-16 19:22:12 +0530

    add:href to navbar_github

--- src/lib/components/navBar.svelte
+++ src/lib/components/navBar.svelte
@@ -4,21 +4,33 @@
 	import { Github } from 'lucide-svelte';
 </script>
 
-<nav class="max-w-6xl mx-auto px-4">
-	<div class="flex flex-row-reverse justify-items-end items-center md:justify-center h-16">
-		<div class="hidden md:flex items-center space-x-4">
-			<a href="https://github.com" class="text-black hover:text-red-500 px-3 py-2 text-lg font-medium border-b-2 border-transparent transition duration-150 ease-in-out underline">Github</a>
+<nav class="mx-auto max-w-6xl px-4">
+	<div class="flex h-16 flex-row-reverse items-center justify-items-end md:justify-center">
+		<div class="hidden items-center space-x-4 md:flex">
+			<a
+				href="https://github.com/Mahathaan/Rants.git"
+				class="border-b-2 border-transparent px-3 py-2 text-lg font-medium text-black underline transition duration-150 ease-in-out hover:text-red-500"
+				>Github</a
+			>
 			<Tooltip.Root openDelay={0} closeDelay={0}>
 				<Tooltip.Trigger>
-					<a href="/" class="text-black hover:text-red-500 px-3 py-2 text-lg font-medium border-b-2 border-transparent transition duration-150 ease-in-out underline">Docs</a>
+					<a
+						href="/"
+						class="border-b-2 border-transparent px-3 py-2 text-lg font-medium text-black underline transition duration-150 ease-in-out hover:text-red-500"
+						>Docs</a
+					>
 				</Tooltip.Trigger>
-				<Tooltip.Content class="bg-white border border-black text-black p-2 rounded-md shadow-md">
+				<Tooltip.Content class="rounded-md border border-black bg-white p-2 text-black shadow-md">
 					<p>Chill, You don't need any of this!</p>
 				</Tooltip.Content>
 			</Tooltip.Root>
 		</div>
 		<div class="md:hidden">
-			<Button variant="outline" href="https://github.com" class="flex items-center space-x-2">
+			<Button
+				variant="outline"
+				href="https://github.com/Mahathaan/Rants.git"
+				class="flex items-center space-x-2"
+			>
 				<Github class="h-5 w-5" />
 				<span>Github</span>
 			</Button>
