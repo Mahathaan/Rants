1ad06df3b3edf22e5e4617844c262547499f41d6
HARI PRASANTH S <hariprasanth581@gmail.com>
2024-10-16 19:33:32 +0530

    add:quote

--- src/data/data.ts
+++ src/data/data.ts
@@ -7,7 +7,7 @@ export const writes: Write[] = [
 		text: 'The product that wins is the one that bridges customers to the future, not the one that requires a giant leap.'
 	},
 	{
-		text: "User experience is everything. It always has been, but it’s still undervalued and under-invested in. If you don’t know user-centered design, study it. Hire people who know it. Obsess over it. Live and breathe it. Get your whole company on board."
+		text: 'User experience is everything. It always has been, but it’s still undervalued and under-invested in. If you don’t know user-centered design, study it. Hire people who know it. Obsess over it. Live and breathe it. Get your whole company on board.'
 	},
 	{
 		text: 'Chase the vision, not the money; the money will end up following you.'
@@ -18,8 +18,15 @@ export const writes: Write[] = [
 	{
 		text: 'I believe a good leader brings out the best in people by listening to them, trusting them, believing in them, respecting them, and letting them have a go.'
 	},
-	{ text: 'If you’re going to put your product in beta – put your business model in beta with it.' },
-	{ text: 'I knew that if I failed I wouldn’t regret that, but I knew the one thing I might regret is not trying.' },
+	{
+		text: 'If you’re going to put your product in beta – put your business model in beta with it.'
+	},
+	{
+		text: 'I knew that if I failed I wouldn’t regret that, but I knew the one thing I might regret is not trying.'
+	},
 	{ text: 'You don’t need to have a 100-person company to develop that idea.' },
-	{ text: 'If you are not embarrassed by the first version of your product, you’ve launched too late.' }
+	{
+		text: 'If you are not embarrassed by the first version of your product, you’ve launched too late.'
+	},
+	{ text: 'Add Your Quote Here' }
 ];
